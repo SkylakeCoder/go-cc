58a43ef12c970c6a617eec450c7ba88e6a74f655
SkylakeCoder <skylakecoder@gmail.com>
2016-10-20 09:55:38 +0800

    add new method in MyList.

--- src/chess/custom_list.go
+++ src/chess/custom_list.go
@@ -2,6 +2,7 @@ package chess
 
 import (
 	"container/list"
+	"log"
 )
 
 type MyList struct {
@@ -12,14 +13,24 @@ func NewMyList() *MyList {
 	return &MyList { list.New() }
 }
 
-func (ml *MyList) PushFrontSlice(slice []ChessBoard) {
+func (ml *MyList) PushFrontSlice(slice []*ChessBoardNode) {
 	for _, v := range slice {
 		ml.PushFront(v)
 	}
 }
 
-func (ml *MyList) PushBackSlice(slice []ChessBoard) {
+func (ml *MyList) PushBackSlice(slice []*ChessBoardNode) {
 	for _, v := range slice {
 		ml.PushBack(v)
 	}
+}
+
+func (ml *MyList) PopFront() *ChessBoardNode {
+	v := ml.Front()
+	ml.Remove(v)
+	value, ok := v.Value.(*ChessBoardNode)
+	if !ok {
+		log.Fatalln("wrong type in MyList...")
+	}
+	return value
 }
